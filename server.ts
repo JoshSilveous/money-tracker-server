9058ace2e552838518ebfcaf470a2189d412f2bb
Joshua Silveous <JoshSilveous@gmail.com>
2023-08-01 11:47:52 -0400

    worked on type system

--- server.ts
+++ server.ts
@@ -1,17 +1,47 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-const app = express()
-const port = 3000
+import jwt from 'jsonwebtoken'
+import { isTypeProfile } from './TypeChecking'
 
-app.use(bodyParser.json())
+console.log(
+	isTypeProfile({ username: 'dsasd', password: 'asdds' }, 'UserCredentials')
+)
 
-app.use((_req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-	next()
-})
+// const jwtSecretKey = 'wcxgVJ1QKCr23Oe72o7Ch8Nyj4itXE2I'
 
-app.listen(port, () => {
-	console.log('listening on port 3000')
-})
+// const payload = {
+// 	userId: 123,
+// 	username: 'exampleuser',
+// 	role: 'admin',
+// }
+// const options = {
+// 	expiresIn: '1h',
+// }
+
+// const token = jwt.sign(payload, jwtSecretKey, options)
+
+// console.log('signed token:')
+// console.log(token)
+
+// try {
+// 	const decodedToken = jwt.verify(token, jwtSecretKey)
+// 	console.log('Decoded Token:', decodedToken)
+// } catch (error) {
+// 	console.error('Error decoding token:', (error as Error).message)
+// }
+
+// const app = express()
+// const port = 3000
+
+// app.use(bodyParser.json())
+
+// app.use((_req, res, next) => {
+// 	res.setHeader('Access-Control-Allow-Origin', '*')
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+// 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+// 	next()
+// })
+
+// app.listen(port, () => {
+// 	console.log('listening on port 3000')
+// })
