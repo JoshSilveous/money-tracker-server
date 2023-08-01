5230b7b560a580c0d06e75684f38a10d01a9b09c
Joshua Silveous <JoshSilveous@gmail.com>
2023-08-01 11:30:26 -0400

    fleshed out runtime typing system

--- TypeChecking.ts
+++ TypeChecking.ts
@@ -1,20 +1,61 @@
-interface TypeProfile {
-	keyNames: string[]
-	keyTypes: string[]
+// UserCredentials
+declare global {
+	interface UserCredentials {
+		username: string
+		password: string
+	}
 }
-function isTypeProfile(object: any, TypeProfile: TypeProfile) {
-	const objectKeys = Object.keys(object)
-	if (JSON.stringify(objectKeys) !== JSON.stringify(TypeProfile.keyNames)) {
-		return false
-	}
+const typeProfiles: TypeProfile[] = [
+	{
+		name: 'UserCredentials',
+		profile: {
+			keyNames: ['username', 'password'],
+			keyTypes: ['string', 'string'],
+		},
+	},
+]
 
-	const objectValueTypes = Object.values(object).map((val) => typeof val)
-	if (
-		JSON.stringify(objectValueTypes) !==
-		JSON.stringify(TypeProfile.keyTypes)
-	) {
-		return false
-	}
+interface TypeProfile {
+	name: string
+	profile: { keyNames: string[]; keyTypes: string[] }
+}
+/**
+ * Checks an object's type, compared to it's TypeProfile (defined in `TypeChecking.ts`)
+ * @param object The object to check
+ * @param typeProfile The TypeProfile's name, should be identicle to `Interface` name.
+ * Error thrown if typeProfile doesn't exist
+ * @returns Boolean
+ */
+export function isTypeProfile(object: any, typeProfile: string) {
+	let thisTypeProfile: TypeProfile | undefined
+	typeProfiles.forEach((typeProf) => {
+		if (typeProf.name === typeProfile) {
+			thisTypeProfile = typeProf
+		}
+	})
 
-	return true
+	if (thisTypeProfile == undefined) {
+		throw Error(`Type Profile ${typeProfile} doesn't exist!`)
+	} else {
+		const objectKeys = Object.keys(object)
+		if (
+			JSON.stringify(objectKeys) !==
+			JSON.stringify(thisTypeProfile.profile.keyNames)
+		) {
+			return false
+		}
+		const objectValueTypes = Object.values(object).map((val) => typeof val)
+		if (
+			JSON.stringify(objectValueTypes) !==
+			JSON.stringify(thisTypeProfile.profile.keyTypes)
+		) {
+			return false
+		}
+
+		return true
+	}
+}
+
+export default {
+	isTypeProfile,
 }
