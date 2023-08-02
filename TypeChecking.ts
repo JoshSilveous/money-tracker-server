876bb6e5a8b3c50223cfd8ce2dbcd7ea9a2bf1c7
Joshua Silveous <JoshSilveous@gmail.com>
2023-08-01 22:58:48 -0400

    added support for multi-type checking at runtime

--- TypeChecking.ts
+++ TypeChecking.ts
@@ -9,6 +9,19 @@ declare global {
 		username: string
 		password: string
 	}
+	interface TokenData {
+		iat: number
+		exp: number
+	}
+	interface UserDataRequest {
+		username: string
+		token: string
+	}
+	interface Transaction {
+		transaction_id: number
+		name: string
+		amount: number
+	}
 }
 const typeProfiles: TypeProfile[] = [
 	{
@@ -25,6 +38,27 @@ const typeProfiles: TypeProfile[] = [
 			keyTypes: ['number', 'string', 'string'],
 		},
 	},
+	{
+		name: 'TokenData',
+		profile: {
+			keyNames: ['iat', 'exp'],
+			keyTypes: ['number', 'number'],
+		},
+	},
+	{
+		name: 'UserDataRequest',
+		profile: {
+			keyNames: ['username', 'token'],
+			keyTypes: ['string', 'string'],
+		},
+	},
+	{
+		name: 'Transaction',
+		profile: {
+			keyNames: ['transaction_id', 'name', 'amount'],
+			keyTypes: ['number', 'string', 'number'],
+		},
+	},
 ]
 
 interface TypeProfile {
@@ -36,30 +70,69 @@ interface TypeProfile {
  * @param object The object to check
  * @param typeProfile The TypeProfile's name, should be identicle to `Interface` name.
  * Error thrown if typeProfile doesn't exist
+ * Union types can be used
+ *
+ * (e.x. `"UserData | TokenData"`)
  * @returns Boolean
  */
 export function isTypeProfile(object: any, typeProfile: string) {
-	let thisTypeProfile: TypeProfile | undefined
-	typeProfiles.forEach((typeProf) => {
-		if (typeProf.name === typeProfile) {
-			thisTypeProfile = typeProf
-		}
-	})
+	// create array from provided typeProfile
+	const inputTypeProfilesArr = typeProfile.split(' & ')
 
-	if (thisTypeProfile == undefined) {
+	let tempTypeProfile: TypeProfile | undefined
+
+	if (inputTypeProfilesArr.length > 1) {
+		// if user provided multiple types
+
+		inputTypeProfilesArr.forEach((inputTypeProf, inputTypeProfIndex) => {
+			// loop over each type provided
+			let typeProfileFound = false
+			typeProfiles.forEach((typeProf) => {
+				// loop over each TypeProfile defined in this file
+
+				if (typeProf.name === inputTypeProf) {
+					// if a matching typeProfile is found
+					typeProfileFound = true
+					if (inputTypeProfIndex === 0) {
+						// for the first provided TypeProfile, create a copy
+						tempTypeProfile = typeProf
+					} else {
+						// for each subsequent TypeProfile, merge the key data to the temporary
+						typeProf.profile.keyNames.forEach((keyName) => {
+							tempTypeProfile!.profile.keyNames.push(keyName)
+						})
+						typeProf.profile.keyTypes.forEach((keyType) => {
+							tempTypeProfile!.profile.keyTypes.push(keyType)
+						})
+					}
+				}
+			})
+			if (!typeProfileFound) {
+				throw Error(`Type Profile ${inputTypeProf} doesn't exist!`)
+			}
+		})
+	} else {
+		typeProfiles.forEach((typeProf) => {
+			if (typeProf.name === inputTypeProfilesArr[0]) {
+				tempTypeProfile = typeProf
+			}
+		})
+	}
+
+	if (tempTypeProfile == undefined) {
 		throw Error(`Type Profile ${typeProfile} doesn't exist!`)
 	} else {
 		const objectKeys = Object.keys(object)
 		if (
 			JSON.stringify(objectKeys) !==
-			JSON.stringify(thisTypeProfile.profile.keyNames)
+			JSON.stringify(tempTypeProfile.profile.keyNames)
 		) {
 			return false
 		}
 		const objectValueTypes = Object.values(object).map((val) => typeof val)
 		if (
 			JSON.stringify(objectValueTypes) !==
-			JSON.stringify(thisTypeProfile.profile.keyTypes)
+			JSON.stringify(tempTypeProfile.profile.keyTypes)
 		) {
 			return false
 		}
