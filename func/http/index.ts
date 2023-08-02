942f8cfb65114237841397210a90fc11461f4fce
Joshua Silveous <JoshSilveous@gmail.com>
2023-08-01 22:58:21 -0400

    added userdata methods

--- func/http/index.ts
+++ func/http/index.ts
@@ -1,6 +1,6 @@
 import handleCreateUserSRC from './functions/handleCreateUser'
 import handleLoginUserSRC from './functions/handleLoginUser'
-
+import handleRequestDataSRC from './functions/handleRequestData'
 /**
  * Handles HTTP Request for `/createuser`
  */
@@ -10,3 +10,8 @@ export const handleCreateUser = handleCreateUserSRC
  * Handles HTTP Request for `/loginuser`
  */
 export const handleLoginUser = handleLoginUserSRC
+
+/**
+ * Handles HTTP Request for `/requestdata`
+ */
+export const handleRequestData = handleRequestDataSRC
