5d48b74d5b177ff885e7b54feea731fb41f0fde9
Joshua Silveous <JoshSilveous@gmail.com>
2023-08-02 21:37:07 -0400

    updated transaction functions

--- func/database/functions/insertTransaction.ts
+++ func/database/functions/insertTransaction.ts
@@ -4,13 +4,20 @@ import { getUserFilePath } from '..'
 function insertTransaction(user_id: number, transaction: NewTransaction) {
 	const db = new SQLite(getUserFilePath(user_id))
 	const sql = `
-        INSERT INTO transactions (name, amount) VALUES (?, ?);
+        INSERT INTO transactions 
+			(name, timestamp, notes, amount, category_id, account_id) 
+			VALUES (?, ?, ?, ?, ?, ?);
     `
 	const stmt = db.prepare(sql)
 	const res = stmt.run(
 		transaction.name,
-		Math.round(transaction.amount * 100) / 100
+		Math.round(transaction.amount * 100) / 100,
+		transaction.notes === undefined ? null : transaction.notes,
+		transaction.amount,
+		transaction.category_id === undefined ? null : transaction.category_id,
+		transaction.account_id === undefined ? null : transaction.account_id,
 	)
+	// stopped here
 	db.close()
 	return res.lastInsertRowid as number
 }
--- func/database/scripts/NEW_USER_TABLE.ts
+++ func/database/scripts/NEW_USER_TABLE.ts
@@ -1,8 +1,33 @@
 const NEW_USER_TABLE = `
+    CREATE TABLE categories (
+        category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL,
+        description VARCHAR
+    );
+    CREATE TABLE accounts (
+        account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL,
+        description VARCHAR
+    );
     CREATE TABLE transactions (
         transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name VARCHAR,
-        amount INTEGER
+        name VARCHAR NOT NULL,
+        timestamp DATETIME,
+        notes VARCHAR,
+        amount INTEGER NOT NULL,
+        category_id INTEGER,
+        account_id INTEGER,
+        FOREIGN KEY (category_id) REFERENCES categories(category_id),
+        FOREIGN KEY (account_id) REFERENCES categories(account_id)
+    );
+    CREATE TABLE earnings (
+        earning_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL,
+        timestamp DATETIME,
+        notes VARCHAR,
+        amount INTEGER NOT NULL,
+        account_id INTEGER,
+        FOREIGN KEY (account_id) REFERENCES categories(account_id)
     );
 `
 export default NEW_USER_TABLE
