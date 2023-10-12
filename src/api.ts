375fc4fddf13401d6b27dc727cdc19d4ff41321e
Joshua Silveous <JoshSilveous@gmail.com>
2023-10-12 14:14:58 -0400

    added api routes and CORS support

--- src/api.ts
+++ src/api.ts
@@ -1,38 +1,39 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import * as httpFunc from './func/http'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use((_req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*')
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 	next()
 })
-
 app.use(bodyParser.json())
 
-app.post('/createuser', httpFunc.handleCreateUser)
-app.post('/loginuser', httpFunc.handleLoginUser)
-app.get('/requestdata', httpFunc.handleRequestData)
-app.post('/deleteuser', httpFunc.handleDeleteUser)
+app.post('/api/createuser', httpFunc.handleCreateUser)
+app.post('/api/loginuser', httpFunc.handleLoginUser)
+app.get('/api/requestdata', httpFunc.handleRequestData)
+app.post('/api/deleteuser', httpFunc.handleDeleteUser)
 
-app.post('/getdisplaydata', httpFunc.handleGetDisplayData)
+app.post('/api/getdisplaydata', httpFunc.handleGetDisplayData)
 
-app.post('/gettransaction', httpFunc.handleGetTransaction)
-app.post('/inserttransaction', httpFunc.handleInsertTransaction)
-app.post('/updatetransaction', httpFunc.handleUpdateTransaction)
-app.post('/deletetransaction', httpFunc.handleDeleteTransaction)
+app.post('/api/gettransaction', httpFunc.handleGetTransaction)
+app.post('/api/inserttransaction', httpFunc.handleInsertTransaction)
+app.post('/api/updatetransaction', httpFunc.handleUpdateTransaction)
+app.post('/api/deletetransaction', httpFunc.handleDeleteTransaction)
 
-app.post('/getcategory', httpFunc.handleGetCategory)
-app.post('/insertcategory', httpFunc.handleInsertCategory)
-app.post('/updatecategory', httpFunc.handleUpdateCategory)
-app.post('/deletecategory', httpFunc.handleDeleteCategory)
+app.post('/api/getcategory', httpFunc.handleGetCategory)
+app.post('/api/insertcategory', httpFunc.handleInsertCategory)
+app.post('/api/updatecategory', httpFunc.handleUpdateCategory)
+app.post('/api/deletecategory', httpFunc.handleDeleteCategory)
 
-app.post('/getaccount', httpFunc.handleGetAccount)
-app.post('/insertaccount', httpFunc.handleInsertAccount)
-app.post('/updateaccount', httpFunc.handleUpdateAccount)
-app.post('/deleteaccount', httpFunc.handleDeleteAccount)
+app.post('/api/getaccount', httpFunc.handleGetAccount)
+app.post('/api/insertaccount', httpFunc.handleInsertAccount)
+app.post('/api/updateaccount', httpFunc.handleUpdateAccount)
+app.post('/api/deleteaccount', httpFunc.handleDeleteAccount)
 
 export { app }
