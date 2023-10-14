7160e54e0818baed0f1bea9ad1d5d9b7bfc3bdea
Joshua Silveous <JoshSilveous@gmail.com>
2023-10-14 12:53:48 -0400

    added username/password validation on back-end

--- src/func/http/handlers/User/handleCreateUser.ts
+++ src/func/http/handlers/User/handleCreateUser.ts
@@ -13,7 +13,41 @@ const handleCreateUser: RequestHandler = function (req, res) {
 
 	const data = req.body as UserCredentials
 
-	// check password strength (NOT IMPLEMENTED YET)
+	// check username for illegal characters
+	const cleanedUsername = data.username.replace(
+		/[!@#$%^&*(){}[\]<>\/\\'\"|?=+~`:,; \t\n\r]/g,
+		''
+	)
+	if (data.username !== cleanedUsername) {
+		res.statusCode = 406
+		res.statusMessage = 'ERROR_REQUEST_FORMAT'
+		res.send()
+	}
+
+	// check password for illegal characters
+	const cleanedPassword = data.password.replace(
+		/[!@#$%^&*(){}[\]<>\/\\'\"|?=+~`:,; \t\n\r]/g,
+		''
+	)
+	if (data.password !== cleanedPassword) {
+		res.statusCode = 406
+		res.statusMessage = 'ERROR_REQUEST_FORMAT'
+		res.send()
+	}
+
+	// check username length
+	if (data.username.length <= 8 || data.username.length > 30) {
+		res.statusCode = 406
+		res.statusMessage = 'ERROR_REQUEST_FORMAT'
+		res.send()
+	}
+
+	// check password length
+	if (data.password.length <= 8 || data.password.length > 30) {
+		res.statusCode = 406
+		res.statusMessage = 'ERROR_REQUEST_FORMAT'
+		res.send()
+	}
 
 	try {
 		// attempt creating user in database
