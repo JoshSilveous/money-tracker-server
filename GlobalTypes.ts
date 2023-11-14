05614655ef4b9ab8802655b556b43be3a47f2024
Joshua Silveous <JoshSilveous@gmail.com>
2023-11-13 23:27:24 -0500

    modified express Request type

--- GlobalTypes.ts
+++ GlobalTypes.ts
@@ -1,3 +1,10 @@
+declare module 'express-serve-static-core' {
+	// allow appending user_id and username for authenticated requests
+	interface Request {
+		user_id: number
+		username: string
+	}
+}
 declare global {
 	interface UserCredentials {
 		username: string
